a23776a743535171dcd9e4fede0e6fc9bcc48f06
Yusuf Berki YAZICIOĞLU <32420086+YusufBerki@users.noreply.github.com>
2021-07-31 22:39:04 +0300

    Save prediction results to excel

--- data/data_predictor.py
+++ data/data_predictor.py
@@ -1,4 +1,8 @@
+import os
+
 import numpy as np
+import pandas as pd
+
 
 class Predictor:
     def __init__(self, opt, model, data_generator):
@@ -16,3 +20,20 @@ class Predictor:
         # Prediction Classes
         self.y_pred_classes = self.model.predict(self.data_generator)
         self.y_pred_classes = np.argmax(self.y_pred_classes, axis=1)
+
+        self.label_map = self.data_generator.class_indices
+        self.reversed_label_map = {v: k for k, v in self.data_generator.class_indices.items()}
+
+        self.save_prediction_results()
+
+    def save_prediction_results(self):
+        prediction_results = []
+        for index, value in enumerate(self.y_pred_classes):
+            image_name = self.data_generator.filenames[index]
+            prediction_results.append({
+                "image": image_name,
+                "real": self.reversed_label_map[self.y_test[index]],
+                "prediction": self.reversed_label_map[value]
+            })
+        df = pd.DataFrame(prediction_results)
+        df.to_excel(os.path.join(self.opt.results_dir, 'prediction_results.xlsx'), index=False)
